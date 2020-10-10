5a325e97382b207adec8911efae506e90cbe9efe
Tastigernet <tastiger.mr@gmail.com>
2020-10-10 19:33:47 +1100

    add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+var api = 'http://127.0.0.1:40000';
+var poolListUrl = 'https://raw.githubusercontent.com/Cache-core/Assets/master/cache-pools.json';
+var donationAddress = "";
+var blockTargetInterval = 120;
+var coinUnits = 1000000;
+var symbol = 'CXCHE';
+var refreshDelay = 5000;
