22cb7158b2adc99e0128741cb6dc8936c8260f00
Tastiger <tastiger.mr@gmail.com>
2021-01-30 08:47:14 +0000

    cirrect config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var api = 'https://daemon.explorer.cxche.org';
-var poolListUrl = 'https://services.explorer.cxche/pools/list';
+var poolListUrl = 'https://services.explorer.cxche.org/pools/list';
 var donationAddress = "";
 var blockTargetInterval = 120;
 var coinUnits = 100000;
